7a473db17449466befd4625770f0fc25065444d5
adenkesuma <adenfdfd10@gmail.com>
2023-11-12 11:29:59 +0700

    `feat:` add controller and route for add, update and delete user

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -10,3 +10,49 @@ export const getUsers = async (req, res) => {
         })
     }
 }
+
+export const getUserById = async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id)
+        res.json(user)
+    } catch (err) {
+        res.status(404).json({
+            message: err.message
+        })
+    }
+}
+
+export const addUser = async (req, res) => {
+    const user = new User(req.body)
+
+    try {
+        const insertUser = await user.save()
+        res.status(201).json(insertUser)
+    } catch (err) {
+        res.status(400).json({
+            message: err.message
+        })
+    }
+}
+
+export const editUser = async (req, res) => {
+    try {
+        const updateUser = await User.updateOne({ _id: req.params.id }, { $set: req.body })
+        res.status(200).json(updateUser)
+    } catch (err) {
+        res.status(400).json({
+            message: err.message
+        })
+    }
+}
+
+export const deleteUser = async (req, res) => {
+    try {
+        const removeUser = await User.deleteOne({ _id: req.params.id })
+        res.status(200).json(removeUser)
+    } catch (err) {
+        res.status(400).json({ 
+            message: err.message
+        })
+    }
+}
--- server/routes/UserRoute.js
+++ server/routes/UserRoute.js
@@ -1,8 +1,18 @@
 import express from "express"
-import { getUsers } from "../controllers/UserController.js"
+import { 
+    getUsers,
+    getUserById,
+    addUser,
+    editUser,
+    deleteUser
+} from "../controllers/UserController.js"
 
 const router = express.Router()
 
 router.get("/users", getUsers)
+router.get("/users/:id", getUserById)
+router.post("/users/", addUser)
+router.patch("/users/:id", editUser)
+router.delete("/users/:id", deleteUser)
 
 export default router
